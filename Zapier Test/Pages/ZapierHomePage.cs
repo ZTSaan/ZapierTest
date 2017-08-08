c0bd24dc0502c7cbab9bc290fb2171989588f98a
Hassaan <arfajz@outlook.com>
2017-08-08 00:15:09 -0500

    Zapier Test Sample Code

---
+++ Zapier Test/Pages/ZapierHomePage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zapier_Test.Pages
+{
+    public class ZapierHomePage
+    {
+        public IWebDriver Driver { get; private set; }
+
+        [FindsBy(How = How.ClassName, Using = "header__logo")]
+        public IWebElement ZapierLogo;
+
+        public ZapierHomePage(IWebDriver _driver)
+        {
+            Driver = _driver;
+        }
+    }
+}
---
+++ Zapier Test/Tests/ZapierTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zapier_Test.Pages;
+
+namespace Zapier_Test.Tests
+{
+   
+    [TestFixture]
+    public class ZapierTests
+    {
+        public IWebDriver driver = new ChromeDriver();
+        public ZapierHomePage zapierHomePage;
+        string url = "https://zapier.com/";
+
+        [SetUp]
+
+        public void Initialize()
+        {
+            zapierHomePage = new ZapierHomePage(driver);
+            PageFactory.InitElements(driver, zapierHomePage);
+
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [Test]
+
+        public void ZapierLogoIsVisible()
+        {
+
+            bool logoIsPresent = zapierHomePage.ZapierLogo.Displayed;
+
+            Assert.True(logoIsPresent);
+        }
+
+        [TearDown]
+
+        public void TearDownTests()
+        {
+            driver.Quit();
+        }
+    }
+}
